bd8aec7428adbbc45682d4d897f4c7b830d88b5f
LuukVesters <96244478+LuukVesters@users.noreply.github.com>
2021-12-23 11:17:31 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,17 @@
 
 // Onderwerp: Project portfolio Luuk Vesters
 
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+// const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+// toggleButton.addEventListener('click', () => {
+//   navbarLinks.classList.toggle('active')
+// })
+
+
+
+    function myFunction()
+    { 
+        document.getElementById("WelcomeText").style.display="none";
+        document.getElementById("WelcomeText2").style.display="none";
+    }
